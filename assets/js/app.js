ee347c457d29fb888e34575c2f0996ddfc523823
Mohammad Al Mamun Rokon <rokon@Rokons-MBP.local>
2022-12-11 03:00:10 +0600

    `bug` fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,22 +5,29 @@ const tabbedContent = () => {
     for (let i = 0; i < tab.length; i++) {
         tab[0].classList.add("active");
         tabContent[0].classList.add("active");
-        tab[i].addEventListener('click', (e) => {
-            // removing active class form all tabs
-            for (let i = 0; i < tab.length; i++) {
-                tab[i].classList.remove('active');
-            }
-            // removing active class from all content tab
-            for (let i = 0; i < tabContent.length; i++) {
-               tabContent[i].classList.remove('active');
-            }
-            // adding back the active class when clicked
-            e.target.classList.add('active');
-            // get the data attribute
-            let data_content = e.target.getAttribute("data-tab");
-            // adding active class to the tabcontent that matched with data-content
-            document.getElementById(data_content).classList.add('active');
-        }, false);
+        tab[i].addEventListener(
+            "click",
+            (e) => {
+                // removing active class form all tabs
+                for (let i = 0; i < tab.length; i++) {
+                    tab[i].classList.remove("active");
+                    tab[i].firstElementChild.classList.remove("right-icon");
+                }
+                // removing active class from all content tab
+                for (let i = 0; i < tabContent.length; i++) {
+                    tabContent[i].classList.remove("active");
+                }
+                // adding back the active class when clicked
+                e.target.classList.add("active");
+                e.target.firstElementChild.classList.add("right-icon");
+
+                // get the data attribute
+                let data_content = e.target.getAttribute("data-tab");
+                // adding active class to the tabcontent that matched with data-content
+                document.getElementById(data_content).classList.add("active");
+            },
+            false
+        );
     }
 };
 tabbedContent();
